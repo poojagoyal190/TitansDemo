a89ed4d1eafe98cfcdfbe4f0104bfa45366e7caa
Pooja Goyal <pgoyal@seekasia.com>
2023-03-27 10:17:07 +0530

    add comments

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -11,21 +11,21 @@
 // Adds a command to add steps for tests.
 // If there is an error in the test, writes the last passed step to the array
 //
-Cypress.Commands.add('stepInfo', (message: string) => {
-  let arr = Cypress.env('stepInfo') || [];
-  // Add only last step
-  if (arr.length >= 1) {
-    arr = [];
-  }
-  arr.push(message);
-  Cypress.log({
-    displayName: 'StepInfo',
-    message: `${message}`,
-    consoleProps: () => {
-      return {
-        Step: `${message}`
-      };
-    }
-  });
-  Cypress.env('stepInfo', arr);
-});
+// Cypress.Commands.add('stepInfo', (message: string) => {
+//   let arr = Cypress.env('stepInfo') || [];
+//   // Add only last step
+//   if (arr.length >= 1) {
+//     arr = [];
+//   }
+//   arr.push(message);
+//   Cypress.log({
+//     displayName: 'StepInfo',
+//     message: `${message}`,
+//     consoleProps: () => {
+//       return {
+//         Step: `${message}`
+//       };
+//     }
+//   });
+//   Cypress.env('stepInfo', arr);
+// });
